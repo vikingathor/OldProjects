8ea4f03159c138c714381d7e7669059bc442dde7
vikingathor <154252387+vikingathor@users.noreply.github.com>
2024-10-13 16:22:15 +0200

    C# klient

---
+++ Klient till Centraldatorn/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Klient_till_Centraldatorn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnslut = new System.Windows.Forms.Button();
+            this.btnSänd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAnslut
+            // 
+            this.btnAnslut.Location = new System.Drawing.Point(29, 90);
+            this.btnAnslut.Name = "btnAnslut";
+            this.btnAnslut.Size = new System.Drawing.Size(75, 23);
+            this.btnAnslut.TabIndex = 0;
+            this.btnAnslut.Text = "btnAnslut";
+            this.btnAnslut.UseVisualStyleBackColor = true;
+            this.btnAnslut.Click += new System.EventHandler(this.btnAnslut_Click);
+            // 
+            // btnSänd
+            // 
+            this.btnSänd.Location = new System.Drawing.Point(29, 133);
+            this.btnSänd.Name = "btnSänd";
+            this.btnSänd.Size = new System.Drawing.Size(75, 23);
+            this.btnSänd.TabIndex = 1;
+            this.btnSänd.Text = "btnSänd";
+            this.btnSänd.UseVisualStyleBackColor = true;
+            this.btnSänd.Click += new System.EventHandler(this.btnSänd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 253);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(488, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSänd);
+            this.Controls.Add(this.btnAnslut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnslut;
+        private System.Windows.Forms.Button btnSänd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Klient till Centraldatorn/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Klient_till_Centraldatorn
+{
+    public partial class Form1 : Form
+    {
+        TcpClient Klient = new TcpClient();
+        int port = 12345;
+        List<string> BokLista = new List<string>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Klient.NoDelay = true;
+
+            TextFilInläsning("C:\\Users\\vikto\\Downloads\\avd_texter.txt");
+            UppdateraListBox();
+        }
+        private void TextFilInläsning(string filnamn)
+        {
+            try
+            {
+                string[] rader = File.ReadAllLines(filnamn);
+                BokLista.AddRange(rader);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void UppdateraListBox()
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(BokLista.ToArray());
+        }
+        private void btnAnslut_Click(object sender, EventArgs e)
+        {
+            if (!Klient.Connected) StartConnection();
+            
+                btnAnslut.BackColor = Color.Green;
+            
+
+        }
+
+        public async void StartConnection()
+        {
+            try
+            {
+                IPAddress Adressen = IPAddress.Parse("127.0.0.1");
+                await Klient.ConnectAsync(Adressen, port);
+            }
+            catch (Exception error) { MessageBox.Show(error.Message, Text); return; }
+            {
+                btnAnslut.Enabled = true;
+                btnSänd.Enabled = true;
+            }
+        }
+
+        private void btnSänd_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count> 0)
+            {
+                StartaSändning(listBox1.SelectedItems[0].ToString());
+                BokLista.RemoveAt(listBox1.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                UppdateraListBox();
+            }
+            else
+            {
+                MessageBox.Show("välj ett object att skicka. ", Text);
+            }
+        }
+
+        public async void StartaSändning(string message)
+        {
+            byte[] utdata = Encoding.Unicode.GetBytes(message);
+
+            try
+            {
+                await Klient.GetStream().WriteAsync(utdata, 0, utdata.Length);
+            }
+            catch (Exception error ) { MessageBox.Show(error.Message, Text); return; }
+            {
+
+            }
+        }
+    }
+}
